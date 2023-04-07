35c4694fadfa97c9d37b6b28b545a40c33c5a81b
Steven Fearing <smfearing5@gmail.com>
2023-04-06 23:25:02 -0400

    Complete playRound() function

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+//  0 - Rock
+//  1 - Paper
+//  2 - Scissors
+
 function getComputerChoice() {
-    let num = Math.floor(3 * Math.random());
-    
+    return Math.floor(3 * Math.random());
+}
+
+function intToRPS(num) {
     switch (num) {
-        case 1:
+        case 0:
             return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return "ERROR"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "DRAW!";
+    }
+
+    let compare = computerSelection - playerSelection;
+    let str = ""
+
+    switch (compare) {
+        case 1:
+        case -2:
+            str = `You Lose! ${
+                intToRPS(computerSelection)
+            } beats ${
+                intToRPS(playerSelection)
+            }!`
             break;
         case 2:
-            return "Paper";
+        case -1:
+            str = `You Win! ${
+                intToRPS(playerSelection)
+            } beats ${
+                intToRPS(computerSelection)
+            }!`
             break;
         default:
-            return "Scissors";
+            str = "ERROR";
     }
+    return str;
 }
